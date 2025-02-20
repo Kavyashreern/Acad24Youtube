6b2c57b07dcbdcdec49ee8509fb64ca6d2cca3a1
Kavyashree <kavyarn123@gmail.com>
2025-02-20 23:06:30 +0530

    modified B File

--- B.java
+++ B.java
@@ -2,5 +2,7 @@ public class B{
     public static void main(String[] args){
         A a = new A(1, 2);
         System.out.println(a.x);
+        int z= a.x + a.y;
+        System.out.println(z);
     }
 }
