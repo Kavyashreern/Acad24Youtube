c90983e52a05d565769dfb5570539cae74a76d7f
Kavyashree <kavyarn123@gmail.com>
2025-03-13 01:45:44 +0530

    changes done

--- B.java
+++ B.java
@@ -6,7 +6,8 @@ public class B{
         System.out.println(z);
         C c = new C(3, 4);
         System.out.println("Sum of c1 and c2 is: " + c.sum());
-        
+        System.out.println("Product of c1 and c2 is: " + c.product());
         System.out.println("Is c1 greater than c2? " + c.isGreater());
+
     }
 }
